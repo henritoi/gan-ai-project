bfbabf9afb7b6479665de757c3e5ff8474a50f1b
Henri <henritoi@uef.fi>
2020-11-04 14:10:57 +0200

    implmented argparse

--- main.py
+++ main.py
@@ -1,9 +1,37 @@
+# coding: utf-8
 """
     GAN Project
 """
+import argparse
+
+print("""\
+     ██████╗██╗   ██╗ ██████╗██╗     ███████╗ ██████╗  █████╗ ███╗   ██╗
+    ██╔════╝╚██╗ ██╔╝██╔════╝██║     ██╔════╝██╔════╝ ██╔══██╗████╗  ██║
+    ██║      ╚████╔╝ ██║     ██║     █████╗  ██║  ███╗███████║██╔██╗ ██║
+    ██║       ╚██╔╝  ██║     ██║     ██╔══╝  ██║   ██║██╔══██║██║╚██╗██║
+    ╚██████╗   ██║   ╚██████╗███████╗███████╗╚██████╔╝██║  ██║██║ ╚████║
+    ╚═════╝   ╚═╝    ╚═════╝╚══════╝╚══════╝ ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═══╝
+        """)
+
+parser = argparse.ArgumentParser(description='CycleGAN')
+parser.add_argument('-e', dest='execute', help='Select execution')
+
+args = parser.parse_args()
 
 def main():
-    print('GAN Project')
+    _execute_command(args.execute)
+
+def _execute_command(command):
+    if command == 'train':
+        train()
+    elif command == 'test':
+        test()
+
+def train():
+    print('Train')
+
+def test():
+    print('Test')
 
 if __name__ == '__main__':
     main()
