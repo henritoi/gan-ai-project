8f0ac6056f31671df2e4923cb1c7dc449e89aca4
hermanr <hermanr@uef.fi>
2020-11-05 14:33:51 +0200

    boolean args + data download

--- main.py
+++ main.py
@@ -37,15 +37,14 @@ def main():
         if selection.lower() == 'y':
             __initialize_data()
 
-    __execute_command(args.method, args.dataset, epochs=args.epochs)
+    __execute_command(args.execute, args.dataset)
 
-def __execute_command(command=None, dataset=None, epochs=250):
+def __execute_command(command=None, dataset=None):
     if command == None:
         command = __get_command_selection()
 
     if dataset == None:
-        if command == 'train':
-            dataset = __get_dataset_selection() 
+        dataset = __get_dataset_selection() 
     else:
         if not os.path.isdir('datasets/%s' % dataset):
             print('[Error]: Dataset "%s" was not found!' % (dataset))
@@ -58,18 +57,18 @@ def __execute_command(command=None, dataset=None, epochs=250):
                 dataset = __get_dataset_selection()
 
     if command == 'train':
-        __train(dataset, epochs=epochs)
+        __train(dataset)
 
     elif command == 'test':
         __test(dataset)
 
-def __train(dataset, epochs=250):
+def __train(dataset):
     print('Train with %s' % (dataset.capitalize()))
     gan = CycleGan(dataset)
-    gan.train(epochs=epochs, batch_size=1, sample_interval=1, saving_interval=30)
+    gan.train(epochs=90, batch_size=1, sample_interval=1, saving_interval=30)
     print("Training complete")
 def __test(dataset):
-    print('Test with the latest model trained')
+    print('Test with %s' % (dataset.capitalize()))
     gan = CycleGan(dataset)
     gan.test()
     print("Testing complete")
@@ -80,8 +79,7 @@ def __initialize_data():
 def __initialize_args():
     parser = argparse.ArgumentParser(description='CycleGAN')
     __add_boolean_arg(parser, 'list', help='List downloaded datasets')
-    parser.add_argument('-m', dest='method', help='Select metdod to be used')
-    parser.add_argument('-e', dest='epochs', type=int, help='Number of epochs (default 250)', default=250)
+    parser.add_argument('-e', dest='execute', help='Select metdod to be used')
     parser.add_argument('--data', dest='dataset', help='Select dataset to be used')
     __add_boolean_arg(parser, 'init', help='Download the training data and pictures of Kuopio')
 
