4832f8b8ea70e48e32b6f5f55228075f46aa5c42
hermanr <hermanr@uef.fi>
2020-11-05 11:54:27 +0200

    get discriminator and generator

--- main.py
+++ main.py
@@ -3,7 +3,8 @@
     GAN Project
 """
 import argparse
-from dataloader import Dataloader
+from image_loader import ImageLoader
+from cyclegan import CycleGan
 
 print("""\
      ██████╗██╗   ██╗ ██████╗██╗     ███████╗ ██████╗  █████╗ ███╗   ██╗
@@ -19,6 +20,8 @@ parser.add_argument('-e', dest='execute', help='Select execution')
 
 args = parser.parse_args()
 
+datasets = ["ukiyoe2photo", "vangogh2photo", "cezanne2photo", "monet2photo"]
+
 def main():
     _execute_command(args.execute)
 
@@ -29,11 +32,18 @@ def _execute_command(command):
         _test()
 
 def _train():
-    print('Train')
+    print('Starting training')
+    for dataset in datasets:
+        gan = CycleGan(dataset)
+        gan.train(epochs=90, batch_size=1, sample_interval=1, saving_interval=30)
+    print("Training complete")
 
 def _test():
-    print('Test')
-
+    print('Starting testing')
+    for dataset in datasets:
+        gan = CycleGan(dataset)
+        gan.test()
+    print("All fakes generated")
 if __name__ == '__main__':
     main()
 
